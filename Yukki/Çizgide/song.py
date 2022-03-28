8cf63ce86d35eb44b6b3ebab23f732113aa92b04
Aleyivdiiiiiii <100187817+Aleyivdiiiiiii@users.noreply.github.com>
2022-03-29 02:32:09 +0400

    Update and rename Yukki/Inline/song.py to Yukki/Çizgide/song.py

--- Yukki/Çizgide/song.py
+++ Yukki/Çizgide/song.py
@@ -20,7 +20,7 @@ def song_markup(videoid, duration, user_id, query, query_type):
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Close Search",
+                text="🗑 Axtarışı bağlayın",
                 callback_data=f"forceclose {query}|{user_id}",
             )
         ],
@@ -32,17 +32,17 @@ def song_download_markup(videoid, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⬇️ Get Audio",
+                text="⬇️ Audio əldə edin",
                 callback_data=f"gets audio|{videoid}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⬇️ Get Video",
+                text="⬇️ Video əldə edin",
                 callback_data=f"gets video|{videoid}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Close Menu",
+                text="🗑 Menyunu bağlayın",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
